e3fd2da4cc71587f57979e146a5276a28768343f
Simonnnette Labiran <slab0001@student.monash.edu>
2024-10-18 14:24:53 +1100

    updated layout
    updated layout

---
+++ script.js
@@ -0,0 +1,33 @@
+// Add 'enabled' class to div.submit when any div.option is clicked
+document.querySelectorAll('div.option').forEach(function(option) {
+    option.addEventListener('click', function() {
+      // Toggle the 'highlight' class on the clicked option
+      this.classList.toggle('highlight');
+      
+      // Enable the submit button when an option is clicked
+      document.querySelector('div.submit').classList.add('enabled');
+    });
+  });
+  
+  // When div.submit button is clicked, show a random message and disable further clicks
+  document.querySelector('div.submit').addEventListener('click', function() {
+    // Only proceed if div.submit has the enabled class
+    if (this.classList.contains('enabled')) {
+      // Hide all messages first
+      document.querySelectorAll('div.randomMessage1, div.randomMessage2, div.randomMessage3, div.randomMessage4')
+        .forEach(function(message) {
+          message.classList.remove('active');
+        });
+  
+      // Randomly select one of the four messages
+      const messages = ['div.randomMessage1', 'div.randomMessage2', 'div.randomMessage3', 'div.randomMessage4'];
+      const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+  
+      // Show the randomly selected message
+      document.querySelector(randomMessage).classList.add('active');
+  
+      // Disable the div.submit button by removing the 'enabled' class
+      this.classList.remove('enabled');
+    }
+  });
+  
